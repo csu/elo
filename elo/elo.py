ddf5e9cc0eaf1c29f623bd22e8809bafa4aa91fe
Christopher Su <christophersu9@gmail.com>
2015-06-28 14:04:58 -0700

    calculations

--- elo/elo.py
+++ elo/elo.py
@@ -1,4 +1,5 @@
 import constants
+import math
 
 def get_k_factor(player_rating, player_num_matches):
     if constants.K_FACTOR_MATCHES_THRESHOLD:
@@ -8,3 +9,11 @@ def get_k_factor(player_rating, player_num_matches):
         if player_rating < thres:
             return constants.K_FACTORS[i]
     return constants.K_FACTORS[-1]
+
+def expected_score(rating_function, player_a, player_b):
+    rating_a = rating_function(player_a)
+    rating_b = rating_function(player_b)
+    return 1/float(1+math.pow(10, (rating_b-rating_a)/float(400)))
+
+def updated_rating(original_rating, k_factor, expected, actual):
+    return original_rating+k_factor*(actual-expected)
